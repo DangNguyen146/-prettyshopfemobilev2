aa737dd73b8a1132c42aaff6ec0279e2abbc83ff
dangnguyen146 <19521317@gm.uit.edu.vn>
2023-05-10 10:42:03 +0700

    fix router

---
+++ src/component/MobileCheckouFalse.js
@@ -0,0 +1,11 @@
+const MobileCheckouTrue = () => {
+    return (
+      <div className="container text-center ">
+        <h1>Pay Fails, please back home</h1>
+        <p>Thanks!</p>
+      </div>
+    );
+  };
+  
+  export default MobileCheckouTrue;
+  
--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
 import CheckOutMobile from './component/CheckOutMobile';
 import MobileCheckouTrue from './component/MobileCheckouTrue';
+import MobileCheckouFalse from './component/MobileCheckouFalse.js';
 
 function AppRouter() {
   return (
     <Switch>
       <Route exact path="/checkoutmobile/:token" component={CheckOutMobile} />
-      <Route exact path="/mobilecheckouttrue" component={MobileCheckouTrue} />
+      <Route exact path="payment/success" component={MobileCheckouTrue} />
+      <Route exact path="payment/failed" component={MobileCheckouFalse} />
     </Switch>
   );
 }
